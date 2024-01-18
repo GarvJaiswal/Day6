b959ed1ae7772ef45f3c98b8c188255a100e48d7
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-18 15:52:01 +0530

    [Garv] Add:Added uc3 file

---
+++ uc3/uc3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rn = rnd.Next(0, 3);
+        
+            if(rn == 0)
+            {
+                Console.WriteLine("Wage is 0");
+            }
+            else if(rn == 1)
+            {
+                Console.WriteLine("Wage is " + 8 * 20);
+
+            }
+            else
+            {
+                Console.WriteLine("Wage is " + 8 * 10);
+            }
+            Console.ReadLine();
+        }
+    }
+}
