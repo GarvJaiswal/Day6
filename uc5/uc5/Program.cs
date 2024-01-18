d49c2ee7c020a42bc2c7620ad52262a1a4ca5224
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-18 16:00:48 +0530

    [Garv] Add:Added uc5 file

---
+++ uc5/uc5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter days in month");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wage of the month is:" + n * 20);
+        }
+    }
+}
