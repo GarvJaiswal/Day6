5c4d62529468226c325f04c7bf0f4b5dc96b32e4
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-18 15:46:53 +0530

    [Garv] Add: Added uc1 file

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            if (rnd.NextDouble()== 0)
+                Console.WriteLine("Absent");
+            else
+                Console.WriteLine("Present");
+            Console.ReadLine();
+        }
+    }
+}
