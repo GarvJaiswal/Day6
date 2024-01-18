715c24b86d49ec8727a9c3430b4e4e051f1d6f40
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-18 15:49:20 +0530

    [Garv] Add:Added uc2 file

---
+++ uc2/uc2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wage = 20;
+            int hr = 8;
+            Console.WriteLine(wage * hr);
+            Console.ReadLine();
+        }
+    }
+}
