80ba340a30865b67196f6b5424d3490f8ef4e62e
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-18 16:02:05 +0530

    [Garv] Add:Added uc6 file

---
+++ uc6/uc6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hr=Convert.ToInt32(Console.ReadLine());
+            int days=Convert.ToInt32(Console.ReadLine());
+            int wage = 0;
+            while(hr<=100 || days <= 20)
+            {
+                wage+=hr*days;
+                hr++; days++;
+            }
+            Console.WriteLine(wage);
+            Console.ReadLine();
+        }
+    }
+}
